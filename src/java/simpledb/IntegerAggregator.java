3c3ae450ee3355f84239bd57eda4aa514098af5e
Xi Han <xihan94@ucla.edu>
2014-05-09 10:01:38 -0700

    Implement IntegerAggregator

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,5 +1,10 @@
 package simpledb;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Vector;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
@@ -22,8 +27,38 @@ public class IntegerAggregator implements Aggregator {
      *            the aggregation operator
      */
 
+    private class IntGroup {
+    	IntField gbField;
+    	Vector<Tuple> tuples;
+    	
+    	public IntGroup(IntField gbField) {
+    		this.gbField = gbField;
+    		this.tuples = new Vector<Tuple>();
+    	}
+    }
+    
+    // Private Variables
+    int gbField;
+    Type gbFieldType; // INT_TYPE or null. Arity of the aggragate relation will depend on this.
+    int aggrField;
+    int tupNum;
+    Op aggrOperator;
+    HashMap<IntField, IntGroup> groups;
+    IntGroup nogroup;
+    String groupName;
+    
+    // If no group, the groups vector will have only one tuple.
+    // Otherwise, it will be filled with the groups.
+    
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+    	this.gbField = gbfield;
+    	this.gbFieldType = gbfieldtype;
+    	this.aggrField = afield;
+    	this.aggrOperator = what;
+    	this.groups = new HashMap<IntField, IntGroup>();
+    	this.nogroup = new IntGroup(null);
+    	this.groupName = null;
     }
 
     /**
@@ -35,6 +70,29 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+    	if (groupName == null && this.gbFieldType != null) {
+    		groupName = tup.getTupleDesc().getFieldName(this.gbField);
+    		if (groupName == null) {
+    			groupName = "null";
+    		}
+    	}
+    	
+    	if (this.gbField == Aggregator.NO_GROUPING || this.gbFieldType == null) {
+    		// Case 1: no grouping.
+    		// Add the tuple to the only group ==> this.nogroup.
+    		this.nogroup.tuples.add(tup);
+    	} else {
+    		// Case 2: grouping.
+    		// If the group for gbField exists, add the tuple into that group.
+    		// Otherwise, create a group and add the tuple.
+    		if (this.groups.containsKey((IntField) tup.getField(this.gbField))) {
+    			this.groups.get((IntField) tup.getField(this.gbField)).tuples.add(tup);
+    		} else {
+    			IntGroup newGroup = new IntGroup((IntField) tup.getField(this.gbField));
+    			newGroup.tuples.add(tup);
+    			this.groups.put((IntField) tup.getField(this.gbField), newGroup);
+    		}
+    	}
     }
 
     /**
@@ -47,8 +105,111 @@ public class IntegerAggregator implements Aggregator {
      */
     public DbIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        return new DbIterator() {
+        	
+        	Iterator<IntGroup> groupIt;
+        	TupleDesc td;
+        	
+			@Override
+			public void open() throws DbException, TransactionAbortedException {
+				// TODO Auto-generated method stub
+				this.groupIt = groups.values().iterator();				
+				if (!groupIt.hasNext()) {
+					throw new DbException("IntegerAggregator: open failed");
+				}
+				
+				if (groupName != null) {
+					Type[] typeAr = {Type.INT_TYPE, Type.INT_TYPE};
+					String[] nameAr = {groupName, aggrOperator.toString()};
+					td = new TupleDesc(typeAr, nameAr);
+				} else {
+					Type[] typeAr = {Type.INT_TYPE};
+					String[] nameAr = {aggrOperator.toString()};
+					td = new TupleDesc(typeAr, nameAr);
+				}
+			}
+
+			@Override
+			public boolean hasNext() throws DbException,
+					TransactionAbortedException {
+				// TODO Auto-generated method stub
+				return this.groupIt.hasNext();
+			}
+
+			@Override
+			public Tuple next() throws DbException,
+					TransactionAbortedException, NoSuchElementException {
+				// TODO Auto-generated method stub
+				IntGroup currentGroup = this.groupIt.next();
+				int acc = 0;
+				
+				if (aggrOperator == Op.AVG) {
+					for (Iterator<Tuple> it = currentGroup.tuples.iterator(); it.hasNext();) {
+						acc += ((IntField) it.next().getField(aggrField)).getValue();
+					}
+					acc /= currentGroup.tuples.size();
+				}
+				
+				if (aggrOperator == Op.COUNT) {
+					for (Iterator<Tuple> it = currentGroup.tuples.iterator(); it.hasNext();) {
+						acc++;
+					}
+				}
+				
+				if (aggrOperator == Op.SUM) {
+					for (Iterator<Tuple> it = currentGroup.tuples.iterator(); it.hasNext();) {
+						acc += ((IntField) it.next().getField(aggrField)).getValue();
+					}
+				}
+				
+				if (aggrOperator == Op.MAX) {
+					acc = Integer.MIN_VALUE;
+					for (Iterator<Tuple> it = currentGroup.tuples.iterator(); it.hasNext();) {
+						int next = ((IntField) it.next().getField(aggrField)).getValue();
+						if (next > acc) {
+							acc = next;
+						}
+					}
+				}
+				
+				if (aggrOperator == Op.MIN) {
+					acc = Integer.MAX_VALUE;
+					for (Iterator<Tuple> it = currentGroup.tuples.iterator(); it.hasNext();) {
+						int next = ((IntField) it.next().getField(aggrField)).getValue();
+						if (next < acc) {
+							acc = next;
+						}
+					}
+				}
+				
+				Tuple rettup = new Tuple(this.td);
+				if (this.td.numFields() == 1) {
+					rettup.setField(0, new IntField(acc));
+				} else {
+					rettup.setField(0, currentGroup.gbField);
+					rettup.setField(1, new IntField(acc));
+				}
+				return rettup;
+			}
+
+			@Override
+			public void rewind() throws DbException,
+					TransactionAbortedException {
+				this.groupIt = groups.values().iterator();
+			}
+
+			@Override
+			public TupleDesc getTupleDesc() {
+				// TODO Auto-generated method stub
+				return this.td;
+			}
+
+			@Override
+			public void close() {
+				// TODO Auto-generated method stub
+				this.groupIt = null;
+			}
+        };
     }
 
 }
